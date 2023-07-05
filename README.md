6735960594cc80745ffec1b6bf6c6664ef613c95
Aleksandr <avolkov@estee.com>
2023-07-05 18:52:21 +0300

    hasql insert ored in progress

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 build problems:
 - sudo apt install zlib1g
-- sudo apt install liblzma-dev      
+- sudo apt install liblzma-dev
+- sudo apt install librdkafka-dev
+- sudo apt install libpq-dev
--- src/Adapter/Storage/Hasql/Order.hs
+++ src/Adapter/Storage/Hasql/Order.hs
@@ -4,98 +4,76 @@
 module Adapter.Storage.Hasql.Order where
 
 import qualified Interfaces.DAO as IN
-import qualified Data.UUID as UUID
-import qualified Domain.Messages as D
-import qualified Domain.User as D
+import qualified Interfaces.DTO as IN
+import qualified Domain.Models as D
 import qualified Hasql.Connection as HConn
 import qualified Hasql.Session as Session
 import Hasql.Statement (Statement)
-import qualified Hasql.TH as TH
-import qualified PostgreSQL.ErrorCodes as PgErr
+-- import qualified PostgreSQL.ErrorCodes as PgErr
 import RIO hiding (trace)
-import qualified Usecase.Interactor as UC
+import qualified Interfaces.Usecases as UC
 import Data.Functor.Contravariant (contramap)
-import Data.Text (Text)
-import qualified Data.Text as T
 import Hasql.Connection
-import Hasql.Decoders
-import Hasql.Encoders
-import Hasql.Query
-import Hasql.Session
+import qualified Hasql.Decoders as DEC
+import qualified Hasql.Encoders as ENC
+import Hasql.Session(run, QueryError)
 import Hasql.Statement
 import Hasql.Transaction
+import Hasql.Transaction.Sessions
+import RIO.Partial (toEnum)
 
-execQuery :: MonadIO m => HConn.Connection -> a -> Statement a b -> m (Either Session.QueryError b)
-execQuery conn param stmt = liftIO $ Session.run (Session.statement param stmt) conn
 
-execTransaction :: MonadIO m => HConn.Connection -> Transaction.Transaction -> m (Either Session.QueryError b)
-execQuery conn transaction = liftIO $ Session.run (Transaction.run transaction) conn
-
-createOrder :: (MonadIO m, UC.Logger m) => IN.NewOrderDTO -> m (Either ErrDAO D.Order)
-createOrder conn newOrder = do
-  result <- execQuery conn (getQueryFields newOrder) insertUserStmt
-  case result of
-    Right _ -> pure Nothing
-    Left (Session.QueryError _ _ (Session.ResultError (Session.ServerError code e _ _ _ ))) ->
-      if code == PgErr.unique_violation
-        then do
-          UC.logDebug e
-          pure $ Just (IN.ErrValidation e)
-        else handleErr e
-    Left e -> handleErr e
-  where
-    getQueryFields :: D.User -> D.Password -> (UUID.UUID, Text, Text, Text)
-    getQueryFields (D.User uid' (D.Name name') (D.Email email')) (D.Password password') = (uid', name', email', password')
-    insertUserStmt =
-      [TH.rowsAffectedStatement|
-                      insert into "users" (uid, name, email, password)
-                      values ($1::uuid, $2::text, $3::text, $4::text)
-                      |]
-    handleErr e = do
-      UC.logDebug e
-      pure $ Just IN.ErrTechnical e
+-- execTransaction :: MonadIO m => HConn.Connection -> Transaction.Transaction -> m (Either Session.QueryError b)
+-- execQuery conn transaction = liftIO $ Session.run (Transaction.run transaction) conn
 
 
 
 -- Encode/Decode functions for PostgreSQL queries
-orderItemDecoder :: Row OrderItem
+orderItemDecoder :: DEC.Row D.OrderItem
 orderItemDecoder =
-  OrderItem
-    <$> column (nonNullable int8)
-    <*> column (nonNullable int8)
-    <*> column (nonNullable int8)
-    <*> column (nonNullable float4)
+  D.OrderItem
+    <$> DEC.column (DEC.nonNullable (D.OrderItemId . fromIntegral <$> DEC.int8))
+    <*> DEC.column (DEC.nonNullable (D.OrderId . fromIntegral <$> DEC.int8))
+    <*> DEC.column (DEC.nonNullable (D.ProductId . fromIntegral <$> DEC.int8))
+    <*> DEC.column (DEC.nonNullable (fromIntegral <$> DEC.int8))
+    <*> DEC.column (DEC.nonNullable DEC.float4)
 
-orderDecoder :: Row Order
+
+orderDecoder :: DEC.Row D.Order
 orderDecoder =
-  Order
-    <$> column (nonNullable int8)
-    <*> column (nonNullable int8)
+  D.Order
+    <$> DEC.column (DEC.nonNullable (fmap (D.OrderId . fromIntegral) DEC.int8))
+    <*> DEC.column (DEC.nonNullable (fmap (D.UserId . fromIntegral) DEC.int8))
+    <*> DEC.column (DEC.nonNullable DEC.timestamptz)
+    <*> DEC.column (DEC.nonNullable (fmap (toEnum . fromIntegral) DEC.int2))
+    -- <*> DEC.column (DEC.nonNullable $ DEC.listArray DEC.nonNullable $  fmap fromIntegral DEC.int8)
+    -- <*> DEC.column (DEC.nonNullable $ DEC.listArray (DEC.nonNullable orderItemDecoder))
 
-orderEncoder :: Order -> Params
+
+orderEncoder :: D.Order -> ENC.Params D.Order
 orderEncoder order =
-  contramap orId (param (nonNullable int8))
-    <> contramap orUserId (param (nonNullable int8))
+  contramap D.orderId (ENC.param (ENC.nonNullable ENC.int8))
+    <> contramap D.orderUserId (ENC.param (ENC.nonNullable ENC.int8))
 
-orderItemEncoder :: OrderItem -> Params
+orderItemEncoder :: D.OrderItem -> ENC.Params D.OrderItem
 orderItemEncoder orderItem =
-  contramap oItOrderId (param (nonNullable int8))
-    <> contramap oItProductId (param (nonNullable int8))
-    <> contramap oItCount (param (nonNullable int8))
-    <> contramap oItProductPrice (param (nonNullable float4))
+  contramap D.orderItemId (ENC.param (ENC.nonNullable ENC.int8))
+    <> contramap D.orderItemProductId (ENC.param (ENC.nonNullable ENC.int8))
+    <> contramap D.orderItemCount (ENC.param (ENC.nonNullable ENC.int8))
+    <> contramap D.orderItemProductPrice (ENC.param (ENC.nonNullable ENC.float4))
 
 -- Function to insert a new order into the database
-insertNewOrder :: Connection -> NewOrderDTO -> IO (Either QueryError ())
+insertNewOrder :: Connection -> IN.NewOrderDTO -> IO (Either QueryError ())
 insertNewOrder conn orderDto =
-  session conn $ do
+  transaction conn $ do
     -- Insert the order and retrieve its ID
-    orderInsertResult <- run (statement (nOrUserId orderDto) (Statement "INSERT INTO \"order\" (user_id) VALUES ($1) RETURNING id" orderEncoder (rowMaybe orderDecoder) True))
+    orderInsertResult <- run (statement (IN.nOrUserId orderDto) (Statement "INSERT INTO \"order\" (user_id) VALUES ($1) RETURNING id" orderEncoder (DEC.rowMaybe orderDecoder) True))
     case orderInsertResult of
-      Left err -> return (Left err)
+      Left err -> condemn >= return (Left err)
       Right (Just order) -> do
         -- Insert the order items
-        let orderItems = nOrOrderItems orderDto
-        itemsInsertResult <- run (statement orderItems (Statement "INSERT INTO order_item (order_id, product_id, count, product_price) VALUES ($1, $2, $3, $4)" (arrayParam (nonNullable $ composite orderItemEncoder)) (unitDecoder) True))
+        let orderItems = IN.nOrOrderItems orderDto
+        itemsInsertResult <- run (statement orderItems (Statement "INSERT INTO order_item (order_id, product_id, count, product_price) VALUES ($1, $2, $3, $4)" (DEC.rowList (DEC.nonNullable $ DEC.composite orderItemEncoder)) DEC.noResult True))
         case itemsInsertResult of
           Left err -> return (Left err)
           Right _ -> return (Right ())
